1dd7379043ee3d494d5b69023682facae9097c67
neeraj7271 <neeraj8829sini@gmail.com>
2023-12-10 21:21:17 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,24 +5,15 @@ import 'firebase/compat/firestore';
 import "firebase/compat/database";
 import "firebase/compat/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-//   authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-//   projectId: "disneyplus-clone-a33d5",
-//   storageBucket: "disneyplus-clone-a33d5.appspot.com",
-//   messagingSenderId: "37918794208",
-//   appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-//   measurementId: "G-DRVLJKWRWG",
-// };
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_VqtiaI0CNH-ZaTb_9cCG1RvJXdIsMeg",
-  authDomain: "disneyplusclone-62304.firebaseapp.com",
-  projectId: "disneyplusclone-62304",
-  storageBucket: "disneyplusclone-62304.appspot.com",
-  messagingSenderId: "401200648573",
-  appId: "1:401200648573:web:44056d2ce023ab6b5dbc14"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
